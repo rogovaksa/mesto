ec864aea6bdc61a9bc9f7272fbab7e6b76d2c84b
Kseniia Rogova <rogovaksa@yandex.ru>
2023-01-30 23:59:54 +0700

    исправлены недочеты в верстке, дописан функционал попапа Редактировать профиль

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,47 @@
+// Объявление переменных, popup Редактировать профиль
+const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+const userName = document.querySelector('.profile__name');
+const description = document.querySelector('.profile__description');
+
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
-const popupOpenButtonElement = document.querySelector('.profile__edit-button');
+const popupFormElement = popupElement.querySelector('.popup__form');
+const userNamePopupInput = popupElement.querySelector('.popup__input_type_name');
+const descriptionPopupInput = popupElement.querySelector('.popup__input_type_descr');
 
+// Открыть попап
 const openPopup = function() {
-    popupElement.classList.add('popup_is-opened');
+    popupElement.classList.add('popup_opened');
 }
-
+// Закрыть попап
 const closePopup = function() {
-    popupElement.classList.remove('popup_is-opened');
+    popupElement.classList.remove('popup_opened');
 }
 
-const closePopupByClickOnOverlay = function(event) {
-    if(event.target === event.currentTarget) {
-    closePopup();
-    }
-}
-
-let formElement = document.querySelector('.profile__info'); 
-let nameInput = formElement.querySelector(".profile__name");
-let jobInput = formElement.querySelector(".profile__description"); 
-
-function handleFormSubmit (evt) {
-    evt.preventDefault();
-    nameInput.textContent = nameInput.value; 
-    jobInput.textContent = jobInput.value;  
-    closePopupFormEdit();
+// Функция Открыть попап с заполнением полей
+function openPopupForm() {
+    openPopup();
+    userNamePopupInput.value = userName.textContent;
+    descriptionPopupInput.value = description.textContent;
 };
 
-popupOpenButtonElement.addEventListener('click', openPopup);
+// Функция Сохранить данные и Закрыть попап
+function submitPopupForm(evt) {
+    evt.preventDefault();
+    userName.textContent = userNamePopupInput.value;
+    description.textContent = descriptionPopupInput.value;
+    closePopup();
+  };
+
+// Доп. фича с вебинара, чтобы попап закрывался при клике вне формы
+// const closePopupByClickOnOverlay = function(event) {
+//     if(event.target === event.currentTarget) {
+//     closePopup();
+//     }
+// }
+
+// Обработчики событий
+popupOpenButtonElement.addEventListener('click', openPopupForm);
 popupCloseButtonElement.addEventListener('click', closePopup);
-popupElement.addEventListener('click', closePopupByClickOnOverlay);
-formElement.addEventListener('submit', handleFormSubmit);
+popupFormElement .addEventListener("submit", submitPopupForm);
+// popupElement.addEventListener('click', closePopupByClickOnOverlay);
